16e52adb00743d62c9913eb25797f8ff48a223b9
Rauf Abbas <rafo.abbas@gmail.com>
2023-07-19 14:06:25 +0400

    update

--- memory-app/src/App.js
+++ memory-app/src/App.js
@@ -3,31 +3,29 @@ import './App.css';
 import MemoryCard from "./comonents/MemoryCard";
 
 const cardList = [
-    {"path": "/img/1.jpeg"},
-    {"path": "/img/2.jpeg"},
-    {"path": "/img/3.jpeg"},
-    {"path": "/img/4.jpeg"},
-    {"path": "/img/5.jpeg"},
-    {"path": "/img/6.jpeg"}
+    {"path": "/img/1.jpeg", match: false},
+    {"path": "/img/2.jpeg", match: false},
+    {"path": "/img/3.jpeg", match: false},
+    {"path": "/img/4.jpeg", match: false},
+    {"path": "/img/5.jpeg", match: false},
+    {"path": "/img/6.jpeg", match: false}
 ];
 
 
 function App() {
     const [cards, setCards] = useState([]);
-
     const [selectedOne, setSelectedOne] = useState(null);
     const [selectedTwo, setSelectedTwo] = useState(null);
-
     const [disabled, setDisabled] = useState(false);
-
+    const [score, setScore] = useState(0);
     const prepareCards = () => {
         const sortedCards = [...cardList, ...cardList]
             .sort(() => 0.5 - Math.random())
             .map((card) => ({...card, id: Math.random()}));
 
         setCards(sortedCards);
-        setSelectedOne(null);
-        setSelectedTwo(null);
+
+        resetState();
     }
 
     useEffect(() => {
@@ -41,15 +39,39 @@ function App() {
 
     useEffect(() => {
         if (selectedOne && selectedTwo) {
+            setScore(prevScore => prevScore + 1)
             setDisabled(true);
+
+            if (selectedOne.path === selectedTwo.path) {
+                setCards(prevCards =>{
+                    return prevCards.map((card) => {
+                        if (card.path === selectedOne.path) {
+                            return {...card, match: true}
+                        }
+                        return card;
+                    });
+                })
+                resetState();
+            }else {
+                setTimeout(() => {
+                    resetState()
+                }, 1000);
+            }
         }
     }, [selectedOne, selectedTwo]);
 
+    const resetState = () => {
+        setSelectedOne(null);
+        setSelectedTwo(null);
+        setDisabled(false);
+        setScore(0);
+    }
 
     return (
         <div className="container">
             <h1>Memory App</h1>
             <button onClick={prepareCards}>Oyunu Başlat</button>
+            <p>{score}</p>
 
             <div className="card-grid">
                 {
@@ -59,6 +81,7 @@ function App() {
                             card={card}
                             key={card.id}
                             handleSelected={handleSelected}
+                            rotated={card == selectedOne || card == selectedTwo || card.match}
                         />
                     ))
                 }
--- memory-app/src/comonents/MemoryCard.js
+++ memory-app/src/comonents/MemoryCard.js
@@ -1,19 +1,18 @@
 import './MemoryCard.css';
 
-const MemoryCard = ({card, handleSelected, disabled, ...props}) => {
+const MemoryCard = ({card, handleSelected, disabled, rotated, ...props}) => {
     const handleClick = (e) => {
-
-        if (disabled) {
-            alert('Iki tane kart seçdiniz zaten, yeni oyun başlatın.');
+        if (! disabled) {
+            handleSelected(card);
         }
-
-        handleSelected(card);
     }
 
     return (
         <div className="card">
-            <img className='cardFront' src={card.path} alt=""/>
-            <img className='cardBack' onClick={handleClick} src="/img/cover.jpeg" alt=""/>
+            <div className={rotated ? 'rotated' : ''}>
+                <img className='cardFront' src={card.path} alt=""/>
+                <img className='cardBack' onClick={handleClick} src="/img/cover.jpeg" alt=""/>
+            </div>
         </div>
     )
 }
